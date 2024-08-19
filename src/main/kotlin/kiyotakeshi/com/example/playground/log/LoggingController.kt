505e36240416472502d2bb8eb827869aacd57de0
takeshi-kiyota <takeshi-kiyota@bm-sms.co.jp>
2024-08-19 23:25:16 +0900

    logback の設定ファイルについて理解した

--- src/main/kotlin/kiyotakeshi/com/example/playground/log/LoggingController.kt
+++ src/main/kotlin/kiyotakeshi/com/example/playground/log/LoggingController.kt
@@ -10,9 +10,20 @@ import org.springframework.web.bind.annotation.RestController
 class LoggingController {
 
     var logger = LoggerFactory.getLogger(LoggingController::class.java)
+//    2024/08/19 21:21:28 INFO  k.c.e.p.log.LoggingController - an info message
+//    2024/08/19 21:21:28 WARN  k.c.e.p.log.LoggingController - a warn message
+//    2024/08/19 21:21:28 ERROR k.c.e.p.log.LoggingController - an error message
+
+    // package 名を string で指定したらログにもそのように出力された
+    // Class object で渡した方が IDE を使った rename にも強いし良さそう
+//    var logger = LoggerFactory.getLogger("kiyotakeshi.com.example.playground.log")
+//    2024/08/19 21:20:38 INFO  k.com.example.playground.log - an info message
+//    2024/08/19 21:20:38 WARN  k.com.example.playground.log - a warn message
+//    2024/08/19 21:20:38 ERROR k.com.example.playground.log - an error message
 
     @GetMapping
     fun index(): String {
+        // LoggerFactory に渡す logger.trace を logging context という
         logger.trace("a trace message")
         logger.debug("a debug message")
         logger.info("an info message")
