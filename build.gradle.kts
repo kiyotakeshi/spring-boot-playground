4df6a1fed5505fe34b57507e36bc519562d7ebfa
takeshi-kiyota <takeshi-kiyota@bm-sms.co.jp>
2024-08-20 00:48:39 +0900

    actuator の動作確認 + ログを JSON 形式にする動作確認

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	implementation("net.logstash.logback:logstash-logback-encoder:8.0")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ src/main/kotlin/kiyotakeshi/com/example/playground/log/json/JsonLoggingController.kt
@@ -0,0 +1,32 @@
+package kiyotakeshi.com.example.playground.log.json
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/json/logging")
+class JsonLoggingController {
+
+    var logger = LoggerFactory.getLogger(JsonLoggingController::class.java)
+
+    @GetMapping
+    fun index(): String {
+        logger.trace("a inner trace message")
+        logger.debug("a inner debug message")
+
+        return "check out the inner logs to see the output..."
+    }
+
+    @GetMapping("/exception")
+    fun exception(): String {
+        try {
+            throw RuntimeException("this is sample runtime exception")
+        } catch (e: Exception) {
+            logger.error("an exception message", e)
+        }
+
+        return "check out the inner logs to see the output..."
+    }
+}
